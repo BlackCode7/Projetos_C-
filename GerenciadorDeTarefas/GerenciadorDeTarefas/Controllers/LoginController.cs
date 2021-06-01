afbfffefa60e5199051a3426f6ebe5a1ac19a752
Anderson Martins <anderson77martins@outlook.com>
2021-06-01 20:22:43 -0300

    Iniciando a construção da API com C_charp

---
+++ GerenciadorDeTarefas/GerenciadorDeTarefas/Controllers/LoginController.cs
@@ -0,0 +1,43 @@
+﻿using GerenciadorDeTarefas.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GerenciadorDeTarefas.Controllers
+{
+    [ApiController] //adicionei ApiContrller
+    //PQ adicionar api na frente da barra? resp - para acessar recursos de site
+    [Route("api/[controller]")] //adicionei Router("api/[controller]")
+    public class LoginController : ControllerBase //adicionei Controller
+    {
+        private readonly ILogger<LoginController> _logger;//Atributos privados usam _na frente da palavra
+        public LoginController(ILogger<LoginController> logger)
+        {
+            //importante pra mostrar os erros da aplicação
+            _logger = logger;
+        }
+
+        //criando metodo de API POST
+        [HttpPost]
+        public IActionResult EfetuarLogin([FromBody] LoginRequisicaoDto requisicao)
+        {
+            try
+            {
+
+            }
+            catch(Exception excecao)
+            {
+                // respostas de erro da API
+                _logger.LogError("Ocorreu um erro ao efetuar login", excecao, requisicao);
+                return StatusCode(StatusCodes.Status500InternalServerError, new ErroRespostaDto() { 
+                    Status = StatusCodes.Status500InternalServerError,
+                    Erro = "Ocorreu um erro ao efetuar o login, tente novamente!"
+                });
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+Criação do repositorio em 01-06-2021
+
+echo "# Projetos_CCharp" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/BlackCode7/Projetos_CCharp.git
+git push -u origin main
+
+…or push an existing repository from the command line
+
+git remote add origin https://github.com/BlackCode7/Projetos_CCharp.git
+git branch -M main
+git push -u origin main...
